c700a4b7c66ab02b1115918fb46b272d9de19f48
Amar <amar.ajanic@edu.fit.ba>
2022-05-02 18:51:19 +0200

    added client side validation

--- SkijanjeWebApp/SkijanjeASPNET_UI/Models/InstruktorPrikazVM.cs
+++ SkijanjeWebApp/SkijanjeASPNET_UI/Models/InstruktorPrikazVM.cs
@@ -1,16 +1,32 @@
 ﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
 
 namespace SkijanjeASPNET.Models
 {
     public class InstruktorPrikazVM
     {
+        [Required]
         public string? Ime { get; set; }
+        [Required]
+
         public string? Prezime { get; set; }
+        [Required]
+
         public DateTime? DatRodj { get; set; }
+        [Required]
+
         public string? Spol { get; set; }
+        [Required]
+
         public string? KontaktTel { get; set; }
+        [Required]
+
         public DateTime? SkijIsk { get; set; }
+        [Required]
+
         public string? Biografija { get; set; }
+        [Required]
+
         public IFormFile? Cv { get; set; }
     }
 }
--- SkijanjeWebApp/SkijanjeASPNET_UI/wwwroot/js/site.js
+++ SkijanjeWebApp/SkijanjeASPNET_UI/wwwroot/js/site.js
@@ -9,7 +9,89 @@
 
 ////document.getElementById("datumKraj").defaultValue = dateNow.setDate(dateNow.getDate() + 6);
 
+$.validator.addMethod(
+    "regex",
+    function (value, element, regexp) {
+        var check = false;
+        return this.optional(element) || regexp.test(value);
+    },
+    "Please check your input."
+);
 
+$("#instruktorDodaj").validate({
+    rules: {
+        ime: {
+            required: true,
+            regex: /^[A-Z][a-zA-Z ]+[A-Z][a-zA-Z]+$/
+        },
+        prezime: {
+            required: true,
+            regex: /^[A-Z][a-zA-Z ]+[A-Z][a-zA-Z]+$/
+        },
+        datRodj: {
+            required: true
+        },
+        spol: {
+            required: true
+        },
+        kontaktTel: {
+            required: true,
+            regex: /^\+[0-9]{3}-[0-9]{2}-[0-9]{3}-[0-9]{3}$/
+        },
+         skijIsk: {
+            required: true
+        },
+        bio: {
+            required: true,
+            regex: /^[a-zA-Z]+$/
+        },
+        cv: {
+            required: true
+        }
 
+    }
+});
 
-}
+$("#ucenikDodaj").validate({
+    rules: {
+        ime: {
+            required: true,
+            regex: /^[A-Z][a-zA-Z ]+[A-Z][a-zA-Z]+$/
+        },
+        prezime: {
+            required: true,
+            regex: /^[A-Z][a-zA-Z ]+[A-Z][a-zA-Z]+$/
+        },
+        datRodj: {
+            required: true
+        },
+        spol: {
+            required: true
+        },
+        kontaktTel: {
+            required: true,
+            regex: /^\+[0-9]{3}-[0-9]{2}-[0-9]{3}-[0-9]{3}$/
+        },
+    }
+});
+
+$("#instrukcijaDodaj").validate({
+    rules: {
+        datum: {
+            required: true
+        },
+        vrijeme: {
+            required: true
+        },
+        brojCas: {
+            required: true
+        },
+        instruktor: {
+            required: true
+        },
+        biljeske: {
+            required: true
+        }
+    }
+});
+
